e0088033fc554dc97b4f3440d1fe95028872ed4f
Othmane Essakhi <othmane.essakhi@epita.fr>
2021-09-22 19:51:33 +0200

    first bug fixed

--- game/player.js
+++ game/player.js
@@ -51,8 +51,8 @@ Player.prototype.displayInfo = function () {
 }
 
 Player.prototype.turnRight = function (angle) {
-    this.direction += angle;
-    this.graphic.rotateOnAxis(new THREE.Vector3(0,0,-1), angle);
+    this.direction -= angle;
+    this.graphic.rotateOnAxis(new THREE.Vector3(0,0,1), -angle);
 };
 
 Player.prototype.turnLeft = function (angle) {
@@ -78,7 +78,7 @@ Player.prototype.move = function () {
 
     this.graphic.position.x = this.position.x;
     this.graphic.position.y = this.position.y;
-    this.graphic.position.z = this.position.z;
+    // this.graphic.position.z = this.position.z;
     
     light1.position.x = this.position.x;
     light1.position.y = this.position.y;
